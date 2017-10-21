42a1c28400753fc7be5647ae9d196dec17f9673d
NotGael <gael@fontenelle.me>
2017-10-21 14:36:58 +0200

    0.2.4

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -424,15 +424,34 @@
                     }
                 ]
             };
+            var infoWindow = new google.maps.InfoWindow({map: map});
+
+            // Try HTML5 geolocation.
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(function(position) {
+                    var pos = {
+                        lat: position.coords.latitude,
+                        lng: position.coords.longitude
+                    };
+                    infoWindow.setPosition(pos);
+                    infoWindow.setContent('Location found.');
+                    map.setCenter(pos);
+                }, function() {
+                    handleLocationError(true, infoWindow, map.getCenter());
+                });
+            } else {
+                // Browser doesn't support Geolocation
+                handleLocationError(false, infoWindow, map.getCenter());
+            }
             var map = new google.maps.Map(document.getElementById("map"), mapOptions);
             var locData = localStorage.getItem('biers');
             var locDataJson = JSON.parse(locData);
 
-            locDataJson.feed.feed.map(function(res){
+            /*locDataJson.feed.feed.map(function(res){
                 var infowindow = new google.maps.InfoWindow({
                   content: contentString
                 });
-                
+
                 var locDataJsonSplitted = res.gps.split(",");
 
                 var marker = new google.maps.Marker({
@@ -440,9 +459,15 @@
                   map: map,
                   data:locData,
                 });
-            })
+            })*/
 
 
+      }
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
       }
     </script>
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBV7PuDFpGBhrG79Aah4jlAXEI26ziN0_o&callback=initMap"></script>
