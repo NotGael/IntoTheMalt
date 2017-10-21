be4f7b91d250df9e0df2841ca34b28a83da9dbd3
NotGael <gael@fontenelle.me>
2017-10-21 06:18:31 +0200

    0.1.2. Get Brewery by state

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,58 +13,72 @@
                             {{ session('status') }}
                         </div>
                     @endif
-                    <table id="beerTable">
-                      <th>id_beers</th>
-                      <th>name_beer</th>
-                      <th>description</th>
-                      <th>id_breweries</th>
-                      <th>id_categories</th>
-                      <th>id_styles</th>
-                      <th>abv</th>
-                      <th>ibu</th>
-                      <th>srm</th>
-                      <th>ebc</th>
-                      <th>images</th>
-                      <th>last_mod</th>
-                      <th>image1</th>
-                      <th>image2</th>
-                      <th>image3</th>
-                      <th>image4</th>
-                      <th>image5</th>
-                      <th>image6</th>
-                      <th>image7</th>
-                      <th>image8</th>
-                      <th>image9</th>
-                      <th>image10</th>
-                    </table>
                     <!-- Front-End Comment Styliser le Form ['url' => 'foo/bar', 'class' => myClass, 'id' => myId]-->
                     <!-- SELECT ... ['placeholder' => 'Pick a size...', 'class' => myClass, 'id' => myId] -->
-                    {!! Form::open(['url' => 'foo/bar']) !!}
+                    <form name="brewerySearch" id="brewerySearch">
                         {!! Form::token() !!}
                         <!-- Province -->
-                        {!! Form::select('size', ['Anvers' => 'Anvers',
-                                                  'Brabant flamand' => 'Brabant flamand',
-                                                  'Brabant wallon' => 'Brabant wallon',
-                                                  'Flandre occidentale' => 'Flandre occidentale',
-                                                  'Flandre orientale' => 'Flandre orientale',
+                        {!! Form::select('state', ['Antwerpen' => 'Anvers',
+                                                  'Vlaams Brabant' => 'Brabant Flamand',
+                                                  'Brabant Wallon' => 'Brabant Wallon',
+                                                  'West-Vlaanderen"' => 'Flandre Occidentale',
+                                                  'Oost-Vlaanderen' => 'Flandre Orientale',
                                                   'Hainaut' => 'Hainaut',
-                                                  'Liège' => 'Liège',
-                                                  'Limbourg' => 'Limbourg',
+                                                  'Lige' => 'Liège',
+                                                  'Limburg' => 'Limbourg',
                                                   'Luxembourg' => 'Luxembourg',
                                                   'Namur' => 'Namur'], null, ['placeholder' => 'Choisi une province ...']) !!}
                                           <!-- distance / checkbox : blonde, brune, ambrée -->
-                        {!! Form::number('number', '50') !!}
+                        {!! Form::number('range', '50') !!}
                         <!-- Range -->
                         {!! Form::macro('myField', function()
                           {
                             return '<!--input type="awesome"-->';
                           });
                         !!}
+                        {!! Form::myField() !!}
                         <!-- Type -->
-                        {!! Form::checkbox('Blonde', 'blonde') !!}
-                        {!! Form::checkbox('Brune', 'brune') !!}
-                        {!! Form::checkbox('Ambrée', 'ambree') !!}
+                        <label>Blonde : </label>
+                        {!! Form::checkbox('ebc', 'blonde') !!}
+                        <label>Brune : </label>
+                        {!! Form::checkbox('ebc', 'brune') !!}
+                        <label>Ambree : </label>
+                        {!! Form::checkbox('ebc', 'ambree') !!}
+                        <label>Stout : </label>
+                        {!! Form::checkbox('ebc', 'stout') !!}
+                        <button id="brewerySearchSubmit">Submit</button>
                     {!! Form::close() !!}
+                    <table id="beerTable">
+                        <th>id_beers</th>
+                        <th>name_beer</th>
+                        <th>description</th>
+                        <th>id_breweries</th>
+                        <th>id_categories</th>
+                        <th>id_styles</th>
+                        <th>abv</th>
+                        <th>ibu</th>
+                        <th>srm</th>
+                        <th>ebc</th>
+                        <th>images</th>
+                        <th>last_mod</th>
+                        <th>image1</th>
+                        <th>image2</th>
+                        <th>image3</th>
+                        <th>image4</th>
+                        <th>image5</th>
+                        <th>image6</th>
+                        <th>image7</th>
+                        <th>image8</th>
+                        <th>image9</th>
+                        <th>image10</th>
+                        <th>name_brewery</th>
+                        <th>address</th>
+                        <th>city</th>
+                        <th>state</th>
+                        <th>country</th>
+                        <th>gps</th>
+                        <th>web</th>
+                    </table>
                 </div>
             </div>
         </div>
